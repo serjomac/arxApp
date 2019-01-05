f020a55282cf08b53368d14f77d1f0ccf1cfdf73
Jonathan <serjomac@espol.edu.ec>
2019-01-05 16:14:38 -0500

    Correccion de pantallas

---
+++ app/src/main/java/app/arxapp/jonathan/arxapp/Registro.java
@@ -0,0 +1,13 @@
+package app.arxapp.jonathan.arxapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class Registro extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
