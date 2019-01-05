9e0b02047966b7ac9d0f087ae0e3fafd86bb30e1
Jonathan <serjomac@espol.edu.ec>
2019-01-05 17:15:29 -0500

    Ventana Lista Invitados enlace

--- app/src/main/java/app/arxapp/jonathan/arxapp/Menu_Residente.java
+++ app/src/main/java/app/arxapp/jonathan/arxapp/Menu_Residente.java
@@ -1,16 +1,31 @@
 package app.arxapp.jonathan.arxapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Menu_Residente extends AppCompatActivity {
+
+    private ImageView botonListaInvitados;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_residente);
 
+
+        botonListaInvitados = (ImageView) (findViewById(R.id.imgListaInvitados));
+        botonListaInvitados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(v.getContext(), Lista_Invitados.class);
+                startActivityForResult(intent1, 0);
+            }
+        });
 
     }
 
@@ -19,5 +34,8 @@ public class Menu_Residente extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return true;
+
+
+
     }
 }
